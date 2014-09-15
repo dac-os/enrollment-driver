dc7dc11e593929b444ef2f0bc21f03691d8ec92c
Lamasu <rafaerthal@gmail.com>
2014-09-15 16:01:51 -0300

    basic driver comunication

--- index.js
+++ index.js
@@ -1,17 +1,72 @@
-var VError, request, async, uri;
-
+var VError, httpRequest, async, uri;
 VError = require('verror');
-request = require('request');
+httpRequest = require('request');
 async = require('async');
 
-/**
-* @method
-* @summary Setups default base uri
-*
-* @param {String} newUri
-*/
 exports.configUri = function (newUri) {
   'use strict';
-  
+
   uri = newUri;
+};
+
+function downloadPages(url, next) {
+  'use strict';
+
+  var lastPage, results, lastLength;
+  results = [];
+  lastLength = 0;
+  lastPage = 0;
+  async.doWhilst(function (next) {
+    return httpRequest({
+      'url'  : uri + url,
+      'json' : true,
+      'qs'   : {'page' : lastPage}
+    }, function (error, res, body) {
+      if (error) {
+        return next(error);
+      }
+      results = results.concat(body);
+      lastLength = body.length;
+      lastPage++;
+      return next();
+    });
+  }, function () {
+    return lastLength > 0;
+  }, function (error) {
+    return next(error, results);
+  });
+}
+
+exports.enrollments = function (user, next) {
+  'use strict';
+
+  return downloadPages('/users/' + user + '/enrollments', next);
+};
+
+exports.enrollment = function (user, id, next) {
+  'use strict';
+
+  return httpRequest({
+    'url'  : uri + '/users/' + user + '/enrollments/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
+  });
+};
+
+exports.requirements = function (user, enrollment, next) {
+  'use strict';
+
+  return downloadPages('/users/' + user + '/enrollments/' + enrollment + '/requirements', next);
+};
+
+exports.requirement = function (user, enrollment, id, next) {
+  'use strict';
+
+  return httpRequest({
+    'url'  : uri + '/users/' + user + '/enrollments/' + enrollment + '/requirements/' + id,
+    'json' : true
+  }, function (error, res, body) {
+    next(error, body);
+  });
 };
